f737bc2335adf3ab2c2ddefef29c333819f7de3a
Mischa S <revmischa@cpan.org>
2011-10-11 02:06:53 -0700

    sample data

--- test-bot-github.pl
+++ test-bot-github.pl
@@ -9,10 +9,10 @@ use Test::Bot::GitHub;
 
 my $bot = Test::Bot::GitHub->new_with_options;
 $bot->configure_test_harness(
-    tests_dir => "$ENV{HOME}/db/mw/t",
+    tests_dir => "/path/to/my/repo",
 );
 $bot->configure_notifications(
     irc_host => 'irc.int80.biz',
-    irc_channel => '#db',
+    irc_channel => '#int80',
 );
 $bot->run;
